88c073299c93b2079177ac2a0a4c8a46b5d38123
henry lay <henry88lay@gmail.com>
2020-05-09 16:37:32 +1000

    fixed up error

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :djs
   has_many :bookings
-  validates :name, :address, presence: true
+  # validates :name, :address, presence: true
 end
