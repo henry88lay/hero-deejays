6964cbcd3eb644407eee0cf12adcbea5fa45c97a
henry lay <henry88lay@gmail.com>
2020-05-12 20:03:41 +1000

    added the show methods for user controllers and tested the view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :address, :phone_number)
+  end
+end
