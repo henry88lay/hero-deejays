3d715cc981ed4a015fc3f540537d48cc2fff7a5b
camilo cuervo <camilo267@hotmail.es>
2020-05-14 21:20:30 +1000

    Fixing the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
-  resources :users, only: [:index, :show] 
-  resources :booking 
+  resources :bookings
   resources :djs
-
 end
