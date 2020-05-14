596f51b9adea9a7ac9c63dc6d70d75b11f6dccc6
Daniel Nguyen <daniel-nguyen@live.com.au>
2020-05-14 16:06:26 +1000

    Working on Booking Controller, in progress

---
+++ app/controllers/booking_controller.rb
@@ -0,0 +1,31 @@
+class BookingController < ApplicationController
+  def index
+    @booking = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @booking = booking.new(booking_params)
+    @booking.user = @user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render 'users/show'
+    end
+  end
+  
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to booking_path(@booking.user)
+  end
+  private
+  
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+end
