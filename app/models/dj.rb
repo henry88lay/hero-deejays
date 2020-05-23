e6757e6ce6c29abe142c659131d8b5dd0cdb7a1d
camilo cuervo <camilo267@hotmail.es>
2020-05-23 10:50:30 +1000

    fixes

--- app/models/dj.rb
+++ app/models/dj.rb
@@ -1,5 +1,7 @@
 class Dj < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :user, foreign_key: :manager_id
+  has_one_attached :image
+  # belong_to :user
   validates :name, :address, :dj_price, :description, :email, presence: true
 end
