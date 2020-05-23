1a6cb7f285f035678bb458862f7b5a34b7a494e5
henry lay <henry88lay@gmail.com>
2020-05-23 10:40:53 +1000

    fixed out the dj model on belongs to

--- app/models/dj.rb
+++ app/models/dj.rb
@@ -1,5 +1,5 @@
 class Dj < ApplicationRecord
   has_many :bookings, dependent: :destroy
-  belong_to :user, foreign_key: :manager_id
+  belongs_to :user, foreign_key: :manager_id
   validates :name, :address, :dj_price, :description, :email, presence: true
 end
