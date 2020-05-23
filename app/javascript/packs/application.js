8cd198efe13bfb159338f80eee0c91cf21d71f17
camilo cuervo <camilo267@hotmail.es>
2020-05-23 14:42:13 +1000

    adding AOS Js to the Dj cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import 'bootstrap';
+
+import AOS from 'aos';
+AOS.init();
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -13,7 +17,6 @@ import { initMapbox } from '../plugins/init_mapbox';
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
 })
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
