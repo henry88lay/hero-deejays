9607c21f0d4c5d410522563f5cec2d228703f2b5
Daniel Nguyen <daniel-nguyen@live.com.au>
2020-05-23 13:00:57 +1000

    Completed Seed File

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -17,7 +17,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/dark-v10'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
       markers.forEach((marker) => {
